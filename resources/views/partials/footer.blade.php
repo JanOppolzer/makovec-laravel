6461a4711b00d6f78a24528a498f088db8219274
Jan Oppolzer <jan@oppolzer.cz>
2023-03-13 13:22:31 +0100

    Fix a few Blade files

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,8 +7,7 @@
         <small class="text-sm">
             <a class="hover:underline text-blue-500"
                 href="{{ __('welcome.pii-link') }}">{{ __('welcome.pii-text') }}</a><br>
-            &copy; 2022&ndash;{{ date('Y') }} <a class="hover:underline text-blue-500"
-                href="https://www.cesnet.cz">CESNET</a>,
+            &copy; 2023 <a class="hover:underline text-blue-500" href="https://www.cesnet.cz">CESNET</a>,
             <a class="hover:underline text-blue-500" href="mailto:info@eduroam.cz">info@eduroam.cz</a>
         </small>
     </p>
